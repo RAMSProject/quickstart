54eeca2c48cc7ffed6316115e5287c4dd99547e3
Dominic Cerquetti <dom@magfest.org>
2015-05-25 16:39:15 -0400

    skip docker install if we already installed it, so we can run 'vagrant provision' multiple times

--- vagrant/vagrant.sh
+++ vagrant/vagrant.sh
@@ -5,8 +5,16 @@
 # WIP. for now we do it by hand, but later might use DockerCompose
 # or something similar to handle this.
 
+command_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
+
 # install docker (need latest version, not one with apt-get install docker.io)
-wget -qO- https://get.docker.com/ | sh
+if command_exists docker || command_exists lxc-docker; then
+	echo "Docker command already exists, skipping docker install"
+else
+	wget -qO- https://get.docker.com/ | sh
+fi
 
 cd /home/vagrant/docker
 
