a41f7db888c753d4268c2082f2fe6125931aaa3c
Dominic Cerquetti <dom@magfest.org>
2015-05-24 23:56:19 -0400

    require docker 1.6

--- build.sh
+++ build.sh
@@ -7,5 +7,12 @@
 # required to build this container
 # cp ~/.ssh/id_rsa .
 
+REQUIRE_DOCKER_VERSION="1.6"
+
+if [ `docker --version | cut -f 3 -d ' ' | cut -f 1-2 -d '.'` != $REQUIRE_DOCKER_VERSION ]; then 
+	echo "ERROR: you don't have the right version of docker ($REQUIRE_DOCKER_VERSION) installed! please upgrade (or fix this script)"
+	exit -1
+fi
+
 # run the rest of the build
 docker build -t magfest/uber context
