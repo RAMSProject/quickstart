eb74c73472b2efb27ed81d42339b13469c9f85db
Dominic Cerquetti <dom@magfest.org>
2015-05-25 10:24:32 -0400

    let vagrant install docker from the site

--- vagrant/vagrant.sh
+++ vagrant/vagrant.sh
@@ -5,10 +5,10 @@
 # WIP. for now we do it by hand, but later might use DockerCompose
 # or something similar to handle this.
 
+# install docker (need latest version, not one with apt-get install docker.io)
+wget -qO- https://get.docker.com/ | sh
+
 cd /home/vagrant/docker
 
 # build the docker container from scratch
-./build.sh
-
-# modify it suitable for development
-./setup-dev.sh
+./build-dev.sh
