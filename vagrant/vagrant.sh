b4be3a860ae4aa8b944ef59bdfc670c2fe83ae49
Dominic Cerquetti <dom@magfest.org>
2015-05-25 17:20:56 -0400

    more docker fixes

--- vagrant/vagrant.sh
+++ vagrant/vagrant.sh
@@ -9,6 +9,10 @@ command_exists() {
 	command -v "$@" > /dev/null 2>&1
 }
 
+# allow us to run docker without sudo
+groupadd docker
+usermod -aG docker vagrant
+
 # install docker (need latest version, not one with apt-get install docker.io)
 if command_exists docker || command_exists lxc-docker; then
 	echo "Docker command already exists, skipping docker install"
@@ -16,7 +20,7 @@ else
 	wget -qO- https://get.docker.com/ | sh
 fi
 
+# build the docker container from scratch, and then modify it for development
+# so that the code is exposed in app/uber/
 cd /home/vagrant/docker
-
-# build the docker container from scratch
 ./build-dev.sh
