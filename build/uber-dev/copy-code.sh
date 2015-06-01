e08c3bb0444817185a28aac0e85a89e4fb78154a
Dom <dom@magfest.org>
2015-05-31 23:17:06 -0400

    dev build will now install pycharm debugging .egg file if it's present in app/ dir

--- build/uber-dev/copy-code.sh
+++ build/uber-dev/copy-code.sh
@@ -10,6 +10,15 @@
 # run this script FROM INSIDE THE CONTAINER
 
 echo "DEVELOPMENT ONLY: Copying code into data volume so you can edit it..."
-
 mv /uber /mnt/app/
 ln -s /mnt/app/uber /uber
+
+
+# part 2, optional: if PyCharm debugger egg is present, go ahead and install it so
+# that pycharm users can do debugging. follow instructions in the Readme
+
+if [ -e /mnt/app/pycharm-debug-py3k.egg ]
+then
+    echo "DEVELOPMENT ONLY: detected PyCharm egg, installing debugger support"
+    /uber/env/bin/easy_install /mnt/app/pycharm-debug-py3k.egg
+fi
